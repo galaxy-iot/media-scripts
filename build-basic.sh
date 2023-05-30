d505deec0bce3852e061f1575bfc46bb0398af87
galaxy-iot <wh8199@foxmail.com>
2023-05-30 08:47:19 +0800

    install bison and flex, use pip to install meson

--- build-basic.sh
+++ build-basic.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-sudo apt install meson build-essential libpython3-dev python-gi-dev python3-pip
+sudo apt install build-essential libpython3-dev python-gi-dev python3-pip bison flex
 
 sudo pip3 install meson 
 sudo pip3 install ninja
--- setenv.sh
+++ setenv.sh
@@ -3,5 +3,6 @@
 source /opt/intel/openvino/setupvars.sh
 export PATH=$PATH:/usr/local/gstreamer/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/gstreamer/lib/x86_64-linux-gnu:/usr/local/gstreamer/lib/x86_64-linux-gnu/gstreamer-1.0
-export PKG_CONFIG_DIR=$PKG_CONFIG_DIR:/usr/local/gstreamer/lib/x86_64-linux-gnu/pkgconfig
+export LIBRARY_PATH=$LIBRARY_PATH:/use/local/gsteamer/lib/x86_64-linux-gnu:/usr/local/gstreamer/lib/x86_64-linux-gnu/gstreamer-1.0
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/gstreamer/lib/x86_64-linux-gnu/pkgconfig
 export GST_PLUGIN_PATH=$GST_PLUGIN_PATH:/usr/local/gstreamer/lib/x86_64-linux-gnu:/usr/local/gstreamer/lib/x86_64-linux-gnu/gstreamer-1.0
